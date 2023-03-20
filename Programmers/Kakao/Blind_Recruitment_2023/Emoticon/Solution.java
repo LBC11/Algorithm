748ce425e2f6b6bc4777a8fe170229d4c31e0e43
LBC <wpdltm4@gmail.com>
2023-03-20 15:33:06 +0900

    temp

--- Programmers/Kakao/Blind_Recruitment_2023/Emoticon/Solution.java
+++ Programmers/Kakao/Blind_Recruitment_2023/Emoticon/Solution.java
@@ -1,8 +1,23 @@
 package Programmers.Kakao.Blind_Recruitment_2023.Emoticon;
 
 public class Solution {
+
+    int[] discount = {10, 20, 30, 40};
+    int[] answer;
+
+    void recursiveCal(int depth, int curr) {
+
+        for(int i=0; i<4; i++) {
+
+        }
+    }
+
     public int[] solution(int[][] users, int[] emoticons) {
-        int[] answer = {};
+
+        int[] ratio = new int[emoticons.length];
+
+        answer = new int[2];
+
         return answer;
     }
 }
---
+++ Programmers/Kakao/Blind_Recruitment_2023/Escape_Maze/Solution.java
@@ -0,0 +1,8 @@
+package Programmers.Kakao.Blind_Recruitment_2023.Escape_Maze;
+
+class Solution {
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        String answer = "";
+        return answer;
+    }
+}
