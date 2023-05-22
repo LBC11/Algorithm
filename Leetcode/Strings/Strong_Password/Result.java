daebde5b37900b7aee84da5458268826d44c5674
LBC <wpdltm4@gmail.com>
2023-05-22 22:12:58 +0900

    Leetcode Strings-String_Password

--- Leetcode/Strings/Strong_Password/Result.java
+++ Leetcode/Strings/Strong_Password/Result.java
@@ -2,12 +2,32 @@ package Leetcode.Strings.Strong_Password;
 
 public class Result {
 
+    static String numbers = "0123456789";
+    static String lower_case = "abcdefghijklmnopqrstuvwxyz";
+    static String upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static String special_characters = "!@#$%^&*()-+";
+
     public static int minimumNumber(int n, String password) {
 
+        boolean[] array = new boolean[4];
 
+        int lack_length = Math.max(6 - n, 0);
 
-        return 1;
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
 
+            if(c >= 48 && c <= 57) array[0] = true;
+            else if(c >= 65 && c <= 90) array[1] = true;
+            else if(c >= 97 && c <= 122) array[2] = true;
+            else array[3] = true;
+        }
+
+        int lack_types = 0;
+        for(int i=0; i<array.length; i++) {
+            if(!array[i]) lack_types++;
+        }
+
+        return Math.max(lack_types, lack_length);
     }
 
     public static void main(String[] args) {
@@ -18,7 +38,6 @@ public class Result {
         System.out.println((int) 'z');
         System.out.println((int) 'A');
         System.out.println((int) 'Z');
-
 
 
     }
