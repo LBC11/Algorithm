cfa5b6a599b333515438bc81b605ce47c99b4fcf
LBC <wpdltm4@gmail.com>
2023-05-25 22:05:38 +0900

    k진수에서 신고 결과 받기

--- Programmers/Kakao/Blind_Recruitment_2022/Decimal_Number_In_K/Solution.java
+++ Programmers/Kakao/Blind_Recruitment_2022/Decimal_Number_In_K/Solution.java
@@ -1,5 +1,16 @@
 package Programmers.Kakao.Blind_Recruitment_2022.Decimal_Number_In_K;
 
+/*
+1. 아이디어
+    Integer.toString 을 이용해 k 진수로 변환
+    split("0") 을 이용해 string 안의 숫자 추충
+    각 숫자가 decimal 인지 검사
+    decimal 검사시 Math.sqrt(num) 까지만 나누어보면 된다.
+
+2. 실패 분석
+    처음에 num 의 크기 상한이 1000000 이길래 array 를 이용해 백만까지의 소수를 미리 구했었다.
+    -> 2진수로 변환시 백만도 매우 큰 string 으로 변환 가능!!!, 심지어 Long 으로 받아야 하는 경우도 생긴다.
+ */
 class Solution {
 
     public int solution(int n, int k) {
