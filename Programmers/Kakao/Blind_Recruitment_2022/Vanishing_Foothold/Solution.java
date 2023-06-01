7952dbbc85f64c7ef7b63e1d3a4ab6ba194fb4a7
LBC <wpdltm4@gmail.com>
2023-06-01 23:15:54 +0900

    temp

---
+++ Programmers/Kakao/Blind_Recruitment_2022/Vanishing_Foothold/Solution.java
@@ -0,0 +1,9 @@
+package Programmers.Kakao.Blind_Recruitment_2022.Vanishing_Foothold;
+
+public class Solution {
+
+    public int solution(int[][] board, int[] aloc, int[] bloc) {
+        int answer = -1;
+        return answer;
+    }
+}
